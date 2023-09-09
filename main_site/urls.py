c9ebed4c813a259ead79cc198242ae0596a9d64e
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-09 14:56:06 -0700

    Created templates for the about page, contact page, menu page and reservation page, all of which extend from the base template.

--- main_site/urls.py
+++ main_site/urls.py
@@ -5,4 +5,8 @@ app_name = 'main_site'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('menu/', views.MenuView.as_view(), name='menu'),
+    path('reserve/', views.ReserveView.as_view(), name='reserve'),
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -7,3 +7,19 @@ class HomeView(generic.TemplateView):
     Display the Home Page
     """
     template_name = 'main/index.html'
+
+
+class AboutView(generic.TemplateView):
+    template_name = 'main/about.html'
+
+
+class ContactView(generic.TemplateView):
+    template_name = 'main/contact.html'
+
+
+class MenuView(generic.TemplateView):
+    template_name = 'main/menu.html'
+
+
+class ReserveView(generic.TemplateView):
+    template_name = 'main/reserve.html'
