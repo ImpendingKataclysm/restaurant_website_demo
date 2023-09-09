42755c12a920ce41ee6b38a3b32ec0672c139f44
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-09 14:10:53 -0700

    Created the Home Page and configured the app settings and urls.

--- main_site/views.py
+++ main_site/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomeView(generic.TemplateView):
+    """
+    Display the Home Page
+    """
+    template_name = 'main/index.html'
