bd3265f599b9970caa1a7a0de74245faa7559801
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-09-10 19:19:30 -0700

    Display the reservation form on the Reservation page. Redirect the user to the home page with a success message when the form is successfully submitted.

--- main_site/views.py
+++ main_site/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-from . import models
+from django.contrib import messages
+from . import models, forms
 
 
 class HomeView(generic.TemplateView):
@@ -48,5 +49,19 @@ class MenuItemDetailView(generic.DetailView):
     template_name = 'main/menu_item_detail.html'
 
 
-class ReserveView(generic.TemplateView):
+class ReserveView(generic.FormView):
+    """
+    Display the reservation form page. When the form is successfully submitted,
+    the reservation is saved in the database and the user is redirected to the
+    home page with a success message.
+    """
+    form_class = forms.ReservationForm
     template_name = 'main/reserve.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        success_message = 'Thanks, we look forward to seeing you!'
+        form.save()
+        messages.success(self.request, success_message)
+
+        return super().form_valid(form)
