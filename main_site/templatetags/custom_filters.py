50522750a25dcf39ebc9ca2b1e1a4fbe2fe25b45
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-12-09 16:13:12 -0800

    Commment custom filter function

--- main_site/templatetags/custom_filters.py
+++ main_site/templatetags/custom_filters.py
@@ -5,6 +5,11 @@ register = template.Library()
 
 @register.filter
 def format_phone_number(phone_number):
+    """
+    Formats a phone number to display it in the standard (xxx) xxx-xxxx format
+    :param phone_number: The non-formatted phone number
+    :return: The formatted phone number
+    """
     digits = [char for char in str(phone_number) if char.isdigit()]
     formatted_phone = "({}{}{}) {}{}{}-{}{}{}{}".format(*digits)
     return formatted_phone
